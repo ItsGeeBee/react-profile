ccf2b9878b039fb64d16ec372dc253900da39b52
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-03 13:01:56 +1100

    Some styling added to project.js, still ugly. Fix if other app functionalites are working

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,17 +7,28 @@ function Project(props) {
   return (
   <div>
     {props.projects.map((project, i) => (
-      <div>
-      <p key={project.id}>{project.title}</p>
-      <p>{project.description}</p>
-      <p>{project.technologies}</p>
-      <a href={props.repo}>GitHub</a>
-      <a href={project.live}>Live Preview</a>
-      <img src={project.image} alt=''></img>
+    <div className="a-box">
+        <div className="img-container">
+        <div className="img-inner">
+        <div className="inner-skew">
+        <img src={project.image} alt=''></img>
+        </div>
+      </div>
     </div>
-    ))}
-  </div>
-  );
-}
+    <div className="text-container">
+        <h4 key={project.id}>{project.title}</h4>
+        <div>
+          <p>{project.description}</p>
+          <p>{project.technologies}</p>
+          <a href={project.repo}>GitHub</a>
+          <br/>
+          <a href={project.live}>Live Preview</a>
+        </div>
+    </div>
+    </div>
+  ))}
+</div>
+  )};
+
 
 export default Project;
