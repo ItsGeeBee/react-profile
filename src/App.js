4abdd46b05c06dc8b6d3e237e59d1de32f256894
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-04 11:10:13 +1100

    restructred routing, trying to resolve nav issue

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
-import React, { useState } from 'react';
-import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Portfolio from './components/Portfolio';
-import Project from './components/Project';
-import Resume from './components/Resume';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Portfolio from "./components/Portfolio";
+import Project from "./components/Project";
+import Resume from "./components/Resume";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-  const pages = [
-    { name: "about" },
+  const [pages] = useState([
+    {
+      name: "about",
+    },
     { name: "portfolio" },
-    { name: "resume" },
+    {
+      name: "resume",
+    },
     { name: "contact" },
-  ];
+  ]);
 
   const [currentPage, setCurrentPage] = useState(pages[0]);
 
-
   return (
     <div className="App">
-      < BrowserRouter>
-      < Header />
-      < Routes>
-      < Route path='/About' element={< About setCurrentPage={setCurrentPage} currentPage={currentPage} pages={pages} />}/>
-      < Route path='/Portfolio' element={< Portfolio/>} />
-      < Route path='/Contact' element={< Contact/>} />
-      < Route path='/Resume' element={< Resume/>} />
-      < Route path='/' element={< About />} />
-      </Routes>
-      < Footer/>
+      <BrowserRouter>
+        <Header>
+          <Nav
+            pages={pages}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          ></Nav>
+        </Header>
+        <Routes>
+          <Route
+            path="/About"
+            element={
+              <About
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+                pages={pages}
+              />
+            }
+          />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Resume" element={<Resume />} />
+          <Route path="/" element={<About />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,13 @@ import './components.css';
 
 function Header(props) {
 
-  
-  return (
-    <div className='header'>
-      <Nav
-      pages={props.pages}
-      setCurrentPage={props.setCurrentPage}
-      currentPage={props.currentPage}
-      />
-      <h1>Grace Brown - A Work in Progress</h1>
-       <h4>Web Development Portfolio</h4>
-    </div>
-  );
-}
+    return (
+      <div className="header">
+        {props.children}
+        <h1>Grace Brown - A Work in Progress</h1>
+        <h4>Web Development Portfolio</h4>
+      </div>
+    )
+  };
+ 
 export default Header;
