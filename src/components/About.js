a5f60c483a502065f52fd79052bcb18767d46b1f
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-04 22:50:44 +1100

    Further styling to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -17,34 +17,16 @@ function About() {
           </h2>
         </div>
         </div> 
-        <div className="flex-container-outer">
-          <img src={Bitmoji} alt="Itsa me!" className="bitmoji"></img>
-            <div id="about-me" className="flex-container-about-me"> With a love of computers from a young age, and a background in customer service, I have learnt how cruical a quaility website can be to a successful business.
+        <div className="row" id="about">
+          <div className="col-4">
+            <img src={Bitmoji} alt="Itsa me!" className="bitmoji"></img>
+          </div>
+          <div id="about-me" className="col-6"> With a love of computers from a young age, and a background in customer service, I have learnt how cruical a quaility website can be to a successful business.
             Please explore my current portfolio to see what kind of work I can help bring to a project.  
             </div>
         </div>
+      </div>
 
-
-  <div className='parallax'>
-  <img src="https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages_1200x.png?v=1619126283" alt=""></img>
-  <article>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </article>
-  </div> 
-
-      {/* <Parallax
-        blur={{ min: -15, max: 15 }}
-        bgImage='https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages_1200x.png?v=1619126283'
-        strength={-100}
-    >
-        <div style={{ height: '500px' }} />
-    </Parallax> */}
-    
-  </div>
   );
 }
 
